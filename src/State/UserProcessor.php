f6cf98b721cd69556c21305e308cd9d4bb44231f
MelhaouiFatima-Zahra <melhaouifatima2000@gmail.com>
2026-03-22 00:24:18 +0000

    Add: UserProcessor + configuration JWT login

--- src/State/UserProcessor.php
+++ src/State/UserProcessor.php
@@ -2,29 +2,38 @@
 
 namespace App\State;
 
+use App\Entity\User;
 use ApiPlatform\Metadata\Operation;
 use ApiPlatform\State\ProcessorInterface;
-use App\Entity\User;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class UserProcessor implements ProcessorInterface
 {
-    public function __construct( 
-        private EntityManagerInterface $entityManager,
-        private UserPasswordHasherInterface $hasher,
-    )
-    {}
+    public function __construct(
+        private EntityManagerInterface $em,
+        private UserPasswordHasherInterface $hasher
+    ) {}
+
     public function process(mixed $data, Operation $operation, array $uriVariables = [], array $context = []): mixed
     {
-        if (!$data instanceof User && $data->getPlainPassword()) {
-            $hashedPassword = $this->hasher->hashPassword($data, $data->getPlainPassword());
-            $data->setPassword($hashedPassword);
-            $data->eraseCredentials();
+        if ($data instanceof User) {
+            if ($data->getPlainPassword() !== null) {
+                $hashedPassword = $this->hasher->hashPassword(
+                    $data,
+                    $data->getPlainPassword()
+                );
+                $data->setPassword($hashedPassword);
+            }
+
+            if (empty($data->getRoles())) {
+                $data->setRoles(['ROLE_USER']);
+            }
         }
-        $this->entityManager->persist($data);
-        $this->entityManager->flush();
-        
+
+        $this->em->persist($data);
+        $this->em->flush();
+
         return $data;
     }
 }
